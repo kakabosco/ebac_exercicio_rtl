b56d3d8939cf426d180add43038a104635cf18b8
Kaique Bosco <kaique.bosco@hotmail.com>
2023-09-23 13:45:10 -0300

    add postComment tests

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,31 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from "@testing-library/react";
+import Post from ".";
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+describe("Teste para o componente Post", () => {
+    it("Deve renderizar o componente corretamente", () => {
+        render(<Post />);
+        expect(screen.getByText("Comentar")).toBeInTheDocument();
+    });
+
+    it("Deve inserir dois comentários", () => {
+        render(<Post />);
+
+        const textarea = screen.getByTestId("post-comment-textarea");
+        fireEvent.change(textarea, {
+            target: { value: "Primeiro comentário via teste" },
+        });
+        const submitButton = screen.getByTestId("post-comment-submit");
+        fireEvent.click(submitButton);
+        expect(
+            screen.getByText("Primeiro comentário via teste")
+        ).toBeInTheDocument();
+
+        fireEvent.change(textarea, {
+            target: { value: "Segundo comentário via teste" },
+        });
+        fireEvent.click(submitButton);
+        expect(
+            screen.getByText("Segundo comentário via teste")
+        ).toBeInTheDocument();
     });
 });
